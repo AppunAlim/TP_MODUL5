7d76e25c6ded183667dab059bcb8c03812ca32a3
AppunAlim <affansalim06@gmail.com>
2026-03-28 11:01:50 +0700

    commit message

--- TP_MODUL5_103022400058/Program.cs
+++ TP_MODUL5_103022400058/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+        DataGeneric<string> dataNIM = new DataGeneric<string>("103022400058");
+        dataNIM.PrintData();
+    }
+}
